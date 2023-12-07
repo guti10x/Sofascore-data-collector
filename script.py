d17906c90ee95a6e1af1d136c08580b5e4d047c2
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2023-12-07 18:52:56 +0100

    Incorporar llamada a Api 2 (partido a scrapear)

--- script.py
+++ script.py
@@ -176,18 +176,19 @@ class SimpleWindow(QDialog):
 
     def invocar_actualizacion(self, nuevo_valor):
         QMetaObject.invokeMethod(self.progress_bar, "setValue", Qt.ConnectionType.QueuedConnection, Q_ARG(int, nuevo_valor))
-        
+       
     def scrapear_funcion(self):
         self.output_textedit.append("Starting scraper...")
 
         # GESTIÓN DEL INPUT DEL USUARIO
+        selected_team = self.team_combobox.currentText()
         ruta_output = self.text_input.text()
+        jornada=int(self.number_input.value())
 
         # Mostrar el valor en el QTextEdit
-        self.output_textedit.append(f"Equipo seleccionado: {self.team_combobox.currentText()}")
-        self.output_textedit.append(f"Jornada seleccionada: {str(self.number_input.value())}")
+        self.output_textedit.append(f"Equipo seleccionado: {selected_team}")
+        self.output_textedit.append(f"Jornada seleccionada: {jornada}")
         self.output_textedit.append(f"Ruta para la salida del scraper selecionada: {ruta_output}")
-        self.output_textedit.append(f"________________________________________________________________________________________")
 
         if ruta_output=="":
             output_textedit = self.output_textedit
@@ -199,7 +200,6 @@ class SimpleWindow(QDialog):
             formato_negro = QTextCharFormat()
             formato_negro.setForeground(QColor(0, 0, 0))
             output_textedit.mergeCurrentCharFormat(formato_negro)
-            self.output_textedit.append(f"________________________________________________________________________________________")
             return
         
         ############################################  eliminar excell a scrapear si ya existe !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -213,9 +213,6 @@ class SimpleWindow(QDialog):
 
         url = "https://sofascore.p.rapidapi.com/teams/search"
 
-        selected_team = self.team_combobox.currentText()
-        self.output_textedit.append(f"Equipo seleccionado: {selected_team}")
-
         id_equipo = None
         valor_round = None
         slugJson = None
@@ -226,10 +223,47 @@ class SimpleWindow(QDialog):
         response = requests.get(url, headers=headers, params={"name": selected_team})
         data = response.json()
         if data['teams']:
-            equipo_id = data['teams'][0]['id']
-            id_equipo=equipo_id
-            self.output_textedit.append(f"GET ID: {selected_team}: {equipo_id}")
+            id_equipo = data['teams'][0]['id']
+            self.output_textedit.append(f"GET ID {selected_team}: {id_equipo}")  
+
+        #######################################################################################################################################################
+        #  PARTE 2 : Mediante una request a la API de Sofaescore obtenemos informacion de todos los equipos de LaLiga                                         #
+        #######################################################################################################################################################
+        time.sleep(1)
+        url = "https://sofascore.p.rapidapi.com/teams/get-last-matches"
+        ultimo_partido_equipos = None
+        liga="LaLiga"
+        ulimo_partido=None
+
+        self.output_textedit.append(f"________________________________________________________________________________________")  
+        self.output_textedit.append(f"Obteniendo partido de la jornada {jornada} del {liga}...")
+
+        querystring = {"teamId": str(id_equipo), "pageIndex": "0"}
+
+        response = requests.get(url, headers=headers, params=querystring)
+
+        if response.status_code == 200:
+            data = response.json()
+            partido=0
+            while True: 
+                valor_round = data['events'][partido]['roundInfo']['round']
+                tournament = data['events'][partido]['tournament']['name']
+                        
+                if valor_round==jornada and tournament==liga:
+                    nombre=data['events'][partido]['slug']
+                    self.output_textedit.append(f"GET partido: {nombre}")
+                    
+                    ulimo_partido=data['events'][partido]
+
+                    valor_round = data['events'][partido]['roundInfo']['round']
+                    break
+                partido+=1
+        else:
+             self.output_textedit.append(f"Error al obtener datos para el equipo {selected_team}. Código de estado: {response.status_code}")
                 
+        nombre_carpeta_jornada = "jornada_" + str(valor_round)
+        ruta_jornada = os.path.join("performance_jugadores_por_partidos", nombre_carpeta_jornada,"json")
+        ##########################################3!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1revisar nombre output
         self.output_textedit.append(f"________________________________________________________________________________________")
 
 if __name__ == '__main__':
